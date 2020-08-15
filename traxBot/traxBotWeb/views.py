6978870a935b0cf8618887559aa8046f967a0dfc
devinlightfoot <euler2171828@gmail.com>
2020-08-14 22:58:26 -0700

    added links to index page

--- traxBot/traxBotWeb/views.py
+++ traxBot/traxBotWeb/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Position
 # Create your views here.
 def index(request):
-    return(HttpResponse("You're at the traxBotWeb index"))
+    return(render(request, 'traxBotWeb/index.html'))
 
 def about(request):
     return(HttpResponse("You're at the about page"))
 
 def  trax(request):
-    return(HttpResponse("You're at the trax page"))
+    pos = Position.objects.all()
+    arr = []
+    for p in pos:
+        temp = '(%f, %f)' % (p.xPos, p.yPos)
+        arr.append(temp)
+    ret = '\n'.join(arr)
+    print(ret)
+    return(HttpResponse(ret))
 
 def profile(request):
     return(HttpResponse("You're at the profile page"))
